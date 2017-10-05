1bde4a9c7ad705b3bb037526e558228c279cc452
abatos <adrian.batos-parac@amdocs.com>
2017-10-05 16:05:06 -0400

    Fix import issue on unit test
    The re-enabled unit test was depending on an older version of the champ
    package so this has been updated to the newest version.
    
    Change-Id: I80c6c8b930280e2a3aa534c73234b1f277fd8b67
    Issue-Id: AAI-295
    Signed-off-by: Adrian Batos <adrian.batos-parac@amdocs.com>#

--- src/test/java/org/openecomp/crud/dao/champ/ChampDaoTest.java
+++ src/test/java/org/openecomp/crud/dao/champ/ChampDaoTest.java
@@ -4,7 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.openecomp.aai.champ.graph.impl.InMemoryChampGraphImpl;
+import org.openecomp.aai.champcore.graph.impl.InMemoryChampGraphImpl;
 import org.openecomp.crud.dao.GraphDao;
 import org.openecomp.crud.entity.Edge;
 import org.openecomp.crud.entity.Vertex;
