4201bd18a0f676121f066670f82be124f9508657
Sotiropoulos, Ioannis (is948x) <ioannis.sotiropoulos@amdocs.com>
2018-02-20 09:47:41 +0000

    Fix junit test
    The relation type wasn't present in the DbEdgeRules file.
    
    Issue-ID: AAI-780
    Change-Id: I1fbcc2b53d9df62b0339605d94ba628f4abd7146
    Signed-off-by: Sotiropoulos, Ioannis (is948x) <ioannis.sotiropoulos@amdocs.com>

--- src/test/java/org/onap/schema/RelationshipSchemaLoaderTest.java
+++ src/test/java/org/onap/schema/RelationshipSchemaLoaderTest.java
@@ -41,7 +41,7 @@ public class RelationshipSchemaLoaderTest {
         RelationshipSchemaLoader.loadModels("v11");
         String version = RelationshipSchemaLoader.getLatestSchemaVersion();
         RelationshipSchema g = RelationshipSchemaLoader.getSchemaForVersion(version);
-        assertNotNull(g.lookupRelationType("org.onap.relationships.inventory.BelongsTo"));
+        assertNotNull(g.lookupRelationType("isMemberOf"));
     }
 
     @Test
