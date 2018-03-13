b6ec637f5ee03c573855431e65fcb6ab0f321851
sblimkie <steven.blimkie@amdocs.com>
2018-03-13 11:52:13 -0400

    Update license date and text
    Update license date and disable license plugin
    
    Change-Id: Ieb54f4fe8dc939bf57af88fb7a71fd510bd20509
    Issue-ID: AAI-865
    Signed-off-by: sblimkie <steven.blimkie@amdocs.com>

--- src/main/java/org/onap/crud/exception/CrudException.java
+++ src/main/java/org/onap/crud/exception/CrudException.java
@@ -1,16 +1,15 @@
 /**
  * ﻿============LICENSE_START=======================================================
- * Gizmo
+ * org.onap.aai
  * ================================================================================
- * Copyright © 2017 AT&T Intellectual Property.
- * Copyright © 2017 Amdocs
- * All rights reserved.
+ * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright © 2017-2018 Amdocs
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,8 +17,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * ============LICENSE_END=========================================================
- *
- * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  */
 package org.onap.crud.exception;
 
