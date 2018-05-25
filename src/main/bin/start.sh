9d48685b916536c5641b2f536e08edba0f9642c3
Daniel Silverthorn <daniel.silverthorn@amdocs.com>
2018-05-25 10:37:42 -0400

    Remove gizmo logback, use mounted one
    Change-Id: I256d2038c554aecc193b96661e080cd3bac82636
    Issue-ID: AAI-1180
    Signed-off-by: Daniel Silverthorn <daniel.silverthorn@amdocs.com>

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APP_HOME="/opt/app/crud-api/"
+APP_HOME="/opt/app/crud-api"
 
 if [ -z "$CONFIG_HOME" ]; then
 	echo "CONFIG_HOME must be set in order to start up process"
@@ -19,6 +19,7 @@ fi
 
 PROPS="-DAPP_HOME=$APP_HOME"
 PROPS="$PROPS -DCONFIG_HOME=$CONFIG_HOME"
+PROPS="$PROPS -Dlogging.config=$APP_HOME/bundleconfig/etc/logback.xml"
 PROPS="$PROPS -DKEY_STORE_PASSWORD=$KEY_STORE_PASSWORD"
 JVM_MAX_HEAP=${MAX_HEAP:-1024}
 
